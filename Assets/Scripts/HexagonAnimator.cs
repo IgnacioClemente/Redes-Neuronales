b18160ec26d0b2e2c77b4ead00e93ea5c1ac1d1c
hansow <nachoagus@live.com.ar>
2019-11-06 00:32:09 -0300

    no message

---
+++ Assets/Scripts/HexagonAnimator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexagonAnimator : MonoBehaviour
+{
+    bool increasingSize = true;
+    Material mat;
+
+	void Start ()
+    {
+        mat = GetComponent<Renderer>().material;
+        mat.color = Color.yellow;
+    }
+	
+
+	void Update ()
+    {
+        float delta = Time.deltaTime;
+        Vector3 angles = transform.eulerAngles;
+        angles.z += delta * 50f;
+        transform.eulerAngles = angles;
+
+        Vector3 localScale = transform.localScale;
+        if (increasingSize == true)
+        {
+            localScale += new Vector3(delta, delta ,0f);
+            if (localScale.x >= 2f)
+            {
+                increasingSize = false;
+            }
+        }
+        else if (increasingSize == false)
+        {
+            localScale -= new Vector3(delta, delta, 0f);
+            if (localScale.x <=1f)
+            {
+                increasingSize = true;
+            }
+        }
+
+        transform.localScale = localScale;
+    }
+}
